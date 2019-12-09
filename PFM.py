72b0e451df3d6e6117959ac92203ebf55b5c741f
hfx532 <a.lobley@qmul.ac.uk>
2019-12-09 10:13:18 +0000

    Update PFM.py

--- PFM.py
+++ PFM.py
@@ -2,26 +2,27 @@
 
 ####### Author
 # Pauline Fourgoux p.f.m.fourgoux@qmul.ac.uk ; pauline.fourgoux@gmail.com
+# alobley@gmail.com
 ####### Packages call
 import pandas as pd
 import sys
 import os
 ####### Funtions
 def usage():
-  print("""
-        computing_RNAseq_AF.py takes the ID of the sample to open a vcf file and calculates the real AF for RNA-seq mutation calling.
-        
-        Example of command line:
-        =======================
-        
-        python computing_RNAseq_AF.py --in-vcf myin.vcf --out-vcf myout.vcf
-        
-        obligatory:
-        ==========
-        
-        --in-vcf  -> VCF
-        --out-vcf -> VCF
-        """)
+    print("""
+    computing_RNAseq_AF.py takes the ID of the sample to open a vcf file and calculates the real AF for RNA-seq mutation calling.
+
+    Example of command line:
+    =======================
+
+    python computing_RNAseq_AF.py --in-vcf myin.vcf --out-vcf myout.vcf
+
+    obligatory:
+    ==========
+
+    --in-vcf  -> VCF
+    --out-vcf -> VCF
+    """)
 
 ####### Main
 # Get arguments
@@ -29,53 +30,45 @@ infile=""
 outfile=""
 #---------------------------------------------------
 try:
-  infile = sys.argv[sys.argv.index("--in-vcf")+1]
-print("Infile: ", infile)
+    infile = sys.argv[sys.argv.index("--in-vcf")+1]
+    print("sample to treat:", sample_ID)
 except:
-  usage()
-print("ERROR : Please enter the input vcf", infile)
-sys.exit()
+    usage()
+    print("ERROR : Please enter the input vcf", infile)
+    sys.exit()
 try:
-  outfile = sys.argv[sys.argv.index("--out-vcf")+1]
-print("Outfile: ", outfile)
+    outfile = sys.argv[sys.argv.index("--out-vcf")+1]
 except:
-  usage()
-print("ERROR : please enter the input vcf file", outfile)
-sys.exit()
+    usage()
+    print("ERROR : please enter the path to out vcf file", outfile)
+    sys.exit()
 
 
-vcf = pd.read_table(infile,skiprows=56)
-rows = vcf.shape[0]
+vcf = pd.read_csv(infile,skiprows=56,sep="\t")
 
-for r in list(range(rows)):
-  #print(vcf.loc[[r]])
-  if vcf.at[r,'FORMAT'] == "GT:AD:DP:GQ:PL":
-  # Retrieving AD and DP to calculate real AF
-  #print(vcf.loc[[r]])
-  sample = vcf.at[r,'sample']
-sample = sample.split(':')
-DP = float(sample[2])
-#print(DP)
-if DP>0:
-  AD = sample[1]
-#print(AD)
-# The comma ',' separates the AD of the different allele.
-# We want to take the last one that is the one of the mutation allele.
-if "," in AD:
-  AD = AD.split(',')
-AD = AD[-1]
-AD = float(AD)
-AF = round(AD/DP,4)
-# Replacing AF in the INFO column
-INFO = vcf.at[r,'INFO']
-INFO = INFO.split(';')
-INFO[1] = "AF=" + str(AF)
-#print(AF)
-#print(INFO[1])
-INFO = ';'.join(INFO)
-vcf.at[r,'INFO'] = INFO
-#print(vcf.loc[[r]])
+for row in vcf:
 
-print("Writing output file:",outfile)
-vcf.to_csv(outfile, '\t', header=True, index=None)
+        fields=vcf.split(":")
+        for i in range(1,len(fields)):
+                if re.match(fields[i],"GT:DP:AF"):
+                        data =  fields[i].split(':')
+                        DP=data.index("DP")
+                        AD=data.index("AD")
+                #print(AD)
+                # The comma ',' separates the AD of the different allele.
+                # We want to take the last one that is the one of the mutation allele.
+                        j=i+1
+                        field=fields[j]
+                        data=field.split(":")
+                        AD = float(data[AD].split(","))
+                        DP = float(data[DP])
+                        AF = round(AD/DP,4)
+                        #---- Replacing AF in the INFO column
+                        INFO = fields[i]+":AF"
+                        field= field+(":").join(INFO)
+                        fields[i]=field
+                        i=len(fields)
+        row=("\t").join(fields)
+        write_csv(row,output)
+
 print("Output written")
